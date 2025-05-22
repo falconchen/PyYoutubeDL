5a0a26680b9d9eaf7397e2eb5e012d578d9db343
falcon <falcon_chen@qq.com>
2025-05-22 12:13:40 +0800

    ✨(bark_util) Add default icon URL to `bark_notify` function
    Include a default icon URL parameter in the `bark_notify` function to enhance notifications.

--- bark_util.py
+++ bark_util.py
@@ -11,10 +11,10 @@ def get_bark_instance(device_token):
             _bark_instance = BarkNotificator(device_token=device_token)
     return _bark_instance
 
-def bark_notify(device_token, title, content):
+def bark_notify(device_token, title, content, icon_url='https://photo.cellmean.com/i/2025/05/22/jyxhwo-0.webp'):
     bark = get_bark_instance(device_token)
     try:
-        bark.send(title=title, content=content)
+        bark.send(title=title, content=content, icon_url=icon_url)
     except Exception as e:
         # 这里可以加日志
         print(f"Bark通知发送失败: {e}")
