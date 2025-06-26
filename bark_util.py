a07066e54eb60404e9e5bf27023a3efe59fed9b0
falcon <falcon_chen@qq.com>
2025-06-26 17:32:29 +0800

    🐛 (bark_util) Fix default icon URL handling
    Load BARK_ICON_URL from config with fallback to default URL. Make icon_url parameter optional.

--- bark_util.py
+++ bark_util.py
@@ -1,8 +1,10 @@
 from BarkNotificator import BarkNotificator
 import threading
+from config_util import load_config
 
 _bark_instance = None
 _bark_lock = threading.Lock()
+config = load_config()
 
 def get_bark_instance(device_token):
     global _bark_instance
@@ -11,7 +13,9 @@ def get_bark_instance(device_token):
             _bark_instance = BarkNotificator(device_token=device_token)
     return _bark_instance
 
-def bark_notify(device_token, title, content, icon_url='https://photo.cellmean.com/i/2025/05/22/jyxhwo-0.webp'):
+def bark_notify(device_token, title, content, icon_url=None):
+    if icon_url is None:
+        icon_url = config.get('BARK_ICON_URL', 'https://photo.cellmean.com/i/2025/05/22/jyxhwo-0.webp')
     bark = get_bark_instance(device_token)
     try:
         bark.send(title=title, content=content, icon_url=icon_url)
